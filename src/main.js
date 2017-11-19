24050e8af5a6833c37393e96fd9f49504ce7cb5b
Gaurav Sharma <gaurav.min.js@gmail.com>
2017-11-19 21:00:38 +0530

    Added data into mockdata file

--- src/main.js
+++ src/main.js
@@ -4,59 +4,21 @@ import * as OfflinePluginRuntime from 'offline-plugin/runtime';
 import './main.scss';
 import Node from './Components/Node/node';
 import Edge from './Components/Edge/edge';
+import mockData from '../src/mockData.json';
 
 OfflinePluginRuntime.install();
 
 class HomePage extends React.Component {
     constructor() {
         super();
-        this.state = {
-            nodeColor: 'rgb(70, 70, 70)',
-            textFontSize: 25,
-            textColor: 'white',
-            edgeColor: 'orange',
-            edgeThickness: '5',
-            nodeRadius: '25',
-            edges: [
-                {
-                    from: 1,
-                    to: 2
-                },
-                {
-                    from: 1,
-                    to: 3
-                }
-            ],
-            nodes: {
-                1: {
-                    id: 1,
-                    cx: 500,
-                    cy: 50,
-                    text: 'A'
-
-                },
-                2: {
-                    id: 2,
-                    cx: 380,
-                    cy: 140,
-                    text: 'B'
-
-                },
-                3: {
-                    id: 2,
-                    cx: 620,
-                    cy: 140,
-                    text: 'C'
-                }
-            }
-        };
+        this.state = Object.assign({}, mockData);
     }
 
     render() {
-        const { nodeColor, textFontSize, textColor, nodes, edges, edgeColor, edgeThickness, nodeRadius } = this.state;
+        const { nodeColor, textFontSize, textColor, nodes, edges, edgeColor, edgeThickness, nodeRadius, containerWidth, containerHeight } = this.state;
         return (
             <div>
-                <svg version="1.1" baseProfile="full" width="1000" height="600" xmlns="http://www.w3.org/2000/svg">
+                <svg version="1.1" baseProfile="full" width={containerWidth} height={containerHeight} xmlns="http://www.w3.org/2000/svg">
                     {/* For every edge there will be a line, and these lines will always be below the nodes. so we should draw lines first and then nodes. */}
                     {edges.map((e, k) => {
                         return <Edge
