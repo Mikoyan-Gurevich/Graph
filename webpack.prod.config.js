3f2cf2d54de31802995104ca19dc97f4fd06962c
Gaurav Sharma <gaurav.min.js@gmail.com>
2017-11-23 00:33:25 +0530

    adding comment in config

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -14,6 +14,7 @@ devConfig.plugins.push(new OfflinePlugin());
 devConfig.plugins.splice(1,1);
 
 
+// gh-pages hosting changes start here 
 devConfig.output.path = path.join(__dirname, 'docs');
 //devConfig.plugins[0] = CleanWebpackPlugin(['docs']);
 devConfig.plugins[3] = new HtmlWebpackPlugin({
@@ -22,5 +23,6 @@ devConfig.plugins[3] = new HtmlWebpackPlugin({
     title: 'Graph JSON',
     template: 'index.ejs'
 });
+// gh-pages hosting changes ends here 
 
 module.exports = devConfig;
